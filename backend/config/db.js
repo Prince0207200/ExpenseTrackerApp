62cdb3811ad30b92ff4efa139526047418c6fa25
Prince <princegupta1455@gmail.com>
2025-03-06 19:42:17 +0530

    changed lock-json reinstall node_module

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const connectDB=async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI,{});
-        console.log("mongodb connected");
-    }catch{
-        console.log("Error while connecting mongodb",err);
-        process.exit(1);
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {});
+        console.log("MongoDB connected");
+    } catch (err) { // Catch the error explicitly
+        console.error("Error while connecting to MongoDB:", err);
+        process.exit(1); // Exit the process with failure
     }
 };
-module.exports=connectDB;
-  
+
+module.exports = connectDB;
